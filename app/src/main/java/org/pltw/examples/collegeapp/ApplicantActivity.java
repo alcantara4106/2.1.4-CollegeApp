9b8b9d02130c86e256e7c898890d1ceed899d5a1
alcantara4106 <alcantara4106@mydusd.org>
2017-03-06 11:56:34 -0800

    Finished 2.1.4 (it works! I think?)

--- app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
+++ app/src/main/java/org/pltw/examples/collegeapp/ApplicantActivity.java
@@ -78,7 +78,7 @@ public class ApplicantActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        FamilyMemberFragment contentFragment = new FamilyMemberFragment();
+        int contentFragment = null;
         int id = item.getItemId();
 
         if (id == R.id.family_member) {
